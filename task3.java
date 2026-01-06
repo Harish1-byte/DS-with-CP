e2f3596ffd9b7f77263462acd8123a61548178eb
Harish1-byte <241801120015@cutmap.ac.in>
2026-01-06 12:14:44 +0530

    Update task3.java

--- task3.java
+++ task3.java
@@ -16,3 +16,38 @@ class Main {
         
     }
 }
+
+
+
+class Node{
+    int data;
+    Node next;
+    //Constructor
+    Node(int a){
+        data=a;
+        next=null;
+    }
+    //method getData
+    public void getData(){
+        System.out.println(data);
+    }
+    public void getRef(){
+        System.out.println(next);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Harish");
+        Node a= new Node(21);
+        Node b= new Node(45);
+        System.out.println(a);
+        System.out.println(b);
+        a.getData();
+        a.getRef();
+        b.getData();
+        b.getRef();
+        a.next = b;
+        a.getRef();
+        
+    }
+}
